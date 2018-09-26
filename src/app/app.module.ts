2409b8908937130c9ece340ec53173f5ede389e3
ext.ddrozd <d.drozd@andersenlab.com>
2018-09-26 21:51:14 +0300

    Добавил material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {HeaderModule} from './header/header.module';
 
-import { AppComponent } from './app.component';
-import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    DashboardModule
+    DashboardModule,
+    BrowserAnimationsModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard.component';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
   declarations: [DashboardComponent],
   exports: [DashboardComponent]
 })

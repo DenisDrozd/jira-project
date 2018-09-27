afe58f9d066600e7a9930347eba671546dd5a21a
ext.ddrozd <d.drozd@andersenlab.com>
2018-09-27 03:49:26 +0300

    Drag & Drop

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TypeTask, CLASS_TASK_COLOR} from '../models/type-task.model';
 
 @Component({
   selector: 'dashboard',
@@ -6,5 +7,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.style.scss']
 })
 export class DashboardComponent {
+  columns = "columns";
 
+  dashboardList = [
+    {
+      title: 'To do',
+      list: [
+        {
+          task: 'JIRA',
+          number: '1',
+          type: TypeTask.TASK,
+          description: 'Here description and otherljs'
+        }
+      ]
+    },
+    {
+      title: 'In progress',
+      list: [
+        {
+          task: 'JIRA',
+          number: '2',
+          type: TypeTask.BUGFIX,
+          description: 'Here2 description and other text'
+        },
+        {
+          task: 'JIRA',
+          number: '3',
+          type: TypeTask.TASK,
+          description: 'Here 3description  fkdsj other text'
+        },
+        {
+          task: 'JIRA',
+          number: '8',
+          type: TypeTask.BUGFIX,
+          description: 'Here4 description and other text'
+        },
+        {
+          task: 'JIRA',
+          number: '9',
+          type: TypeTask.TASK,
+          description: 'Here4 description and other text'
+        },
+        {
+          task: 'JIRA',
+          number: '10',
+          type: TypeTask.SUBTASK,
+          description: 'Here4 description and other text'
+        },
+        {
+          task: 'JIRA',
+          number: '11',
+          type: TypeTask.STORY,
+          description: 'Here4 description and other text'
+        },
+        {
+          task: 'JIRA',
+          number: '12',
+          type: TypeTask.EPIC,
+          description: 'Here4 description and other text'
+        }
+      ]
+    },
+    {
+      title: 'Code review',
+      list: [
+        {
+          task: 'JIRA',
+          number: '5',
+          type: TypeTask.TASK,
+          description: 'Here description and other text'
+        }
+      ]
+    },
+    {
+      title: 'in QA',
+      list: [
+        {
+          task: 'JIRA',
+          number: '6',
+          type: TypeTask.BUGFIX,
+          description: 'Here description and other text'
+        },
+        {
+          task: 'JIRA',
+          number: '33',
+          type: TypeTask.BUGFIX,
+          description: 'Here description and other text'
+        }
+      ]
+    },
+    {
+      title: 'Done',
+      list: [
+        {
+          task: 'JIRA',
+          number: '7',
+          type: TypeTask.TASK,
+          description: 'Here description and other text'
+        }
+      ]
+    }
+  ];
+
+  chooseColorTask(typeColor) {
+    return CLASS_TASK_COLOR[typeColor];
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard.component';
 import {MatCardModule} from '@angular/material/card';
-
+import { DragulaModule } from 'ng2-dragula';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    DragulaModule.forRoot()
   ],
   declarations: [DashboardComponent],
   exports: [DashboardComponent]
